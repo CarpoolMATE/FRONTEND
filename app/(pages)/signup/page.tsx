11ba6cc1047d31561d9b76ce6b546a48ad833ac4
Yong Hyeok Lee <129850728+HelloWorldDogs223@users.noreply.github.com>
2024-12-29 13:30:53 +0900

    init : 프로젝트 초기 설정

---
+++ app/(pages)/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SignupPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Signup Page</h1>
+      {/* Add your signup form components here */}
+    </div>
+  );
+};
+
+export default SignupPage;
