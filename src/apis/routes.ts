91cfcc47251f7d385a904fad313fcb24101af870
heum2 <heum2.dev@gmail.com>
2025-02-09 18:35:49 +0900

    Remove: 스플래쉬 페이지 제거

--- src/apis/routes.ts
+++
@@ -1,6 +0,0 @@
-export const API_ROUTE = {
-  MEMBER: {
-    SIGN_UP: '/api/member/signUp',
-    SIGN_IN: '/api/member/login',
-  },
-};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,8 @@
-"use client";
+import { redirect } from 'next/navigation';
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { APP_ROUTES } from '@/constants/routes';
 
-export default function Splash() {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      router.push("/signin");
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  const router = useRouter();
-  return (
-    <div className="w-[375px] h-[812px] bg-[#007AFF] flex jutsify-center items-center">
-      <h1 className="w-full font-sans text-[90px] text-white flex-1 text-center">
-        mate
-      </h1>
-    </div>
-  );
+// TODO: 유저 정보 확인해서 있으면 메인 아니면 로그인 페이지로 이동
+export default function App() {
+  redirect(APP_ROUTES.SIGNIN);
 }
