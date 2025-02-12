a3957003e3825fcef5fbd7ebea4db4e69028b331
heum2 <heum2.dev@gmail.com>
2025-02-12 21:35:05 +0900

    Chore: tailwind css merge 라이브러리 설치 및 설정

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,14 +3,14 @@ import classNames from 'classnames';
 
 import { ButtonIntentType } from '@/components/button/types';
 
-import { BUTTON_INTENT_CLASSES } from '@/components/button/constants';
+import { cn } from '@/utils/style';
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   intent?: ButtonIntentType;
 };
 
 const Button = ({ intent = 'default', className, ...props }: ButtonProps) => {
-  const buttonClasses = classNames(
+  const buttonClasses = cn(
     'h-[51px] font-pretendard cursor-pointer w-full rounded-xl font-semibold text-lg hover:opacity-95 active:opacity-95 disabled:border-none disabled:bg-[#DADDE1] disabled:cursor-not-allowed disabled:text-[#A2ABB4] disabled:hover:opacity-100',
     BUTTON_INTENT_CLASSES[intent],
     className,
