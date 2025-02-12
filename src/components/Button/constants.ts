a5606f8574049edaa25ebe715596d8909bc732b6
heum2 <heum2.dev@gmail.com>
2025-02-12 23:16:18 +0900

    Feat: 버튼 아이콘 스타일 변경

--- src/components/Button/constants.ts
+++ src/components/Button/constants.ts
@@ -4,5 +4,5 @@ export const BUTTON_INTENT_CLASSES: Record<ButtonIntentType, string> = {
   default: 'bg-primary text-white',
   delete: 'bg-error text-white',
   outline: 'border border-primary text-primary bg-white hover:bg-[#F2F8FF]',
-  icon: 'w-5 h-5 text-center p-2',
+  icon: 'w-fit h-fit text-center',
 };
