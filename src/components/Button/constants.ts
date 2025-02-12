4f44a6c5ee5cf721675c8c98a5642f21ff4b6063
heum2 <heum2.dev@gmail.com>
2025-02-13 01:21:09 +0900

    Feat: 공용 컴포넌트 Input 추가

--- src/components/Button/constants.ts
+++ src/components/Button/constants.ts
@@ -4,5 +4,5 @@ export const BUTTON_INTENT_CLASSES: Record<ButtonIntentType, string> = {
   default: 'bg-primary text-white',
   delete: 'bg-error text-white',
   outline: 'border border-primary text-primary bg-white hover:bg-[#F2F8FF]',
-  icon: 'w-fit h-fit text-center',
+  icon: 'w-fit h-fit text-center text-icon',
 };
