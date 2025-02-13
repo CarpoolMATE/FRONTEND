14fb50dc64d3fdd0a8ff3f29652418d363379046
heum2 <heum2.dev@gmail.com>
2025-02-13 23:52:42 +0900

    Style: 클라이언트 경로로 이름 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import { redirect } from 'next/navigation';
 
-import { APP_ROUTES } from '@/constants/routes';
+import { CLIENT_APP_ROUTES } from '@/constants/routes';
 
 // TODO: 유저 정보 확인해서 있으면 메인 아니면 로그인 페이지로 이동
 export default function App() {
-  redirect(APP_ROUTES.SIGNIN);
+  redirect(CLIENT_APP_ROUTES.SIGNIN);
 }
