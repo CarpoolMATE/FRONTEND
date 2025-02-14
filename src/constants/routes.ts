34337edd1b583fa463e875108e0b6db43fbd71e8
채종원 <cowhddnjsdl0@naver.com>
2025-02-14 13:40:43 +0900

    Design  1. 아이디 찾기 페이지 디자인 재구현  2. 아이디 찾기 결과페이지 반응형 디자인 적용

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -5,6 +5,10 @@ export const API_ROUTES = {
     SIGN_UP: `${BASE_API_URL}/api/member/signUp`,
     SIGN_IN: `${BASE_API_URL}/api/member/signIn`,
   },
+  FIND: {
+    FIND_ID: `${BASE_API_URL}/api/member/findMemberId`,
+    FIND_PASSWORD: `${BASE_API_URL}/api/member/findPassword`,
+  },
 };
 
 export const CLIENT_APP_ROUTES = {
@@ -12,6 +16,7 @@ export const CLIENT_APP_ROUTES = {
   SIGNUP: '/sign-up',
   LOGIN: '/login',
   FIND_ID: '/find-id',
+  FIND_ID_RESULT: '/find-id/result',
   FIND_PASSWORD: '/find-password',
   RESET_PASSWORD: '/reset-password',
   HOME: '/home',
@@ -22,6 +27,7 @@ export const CLIENT_APP_ROUTES_NAME: Record<string, string> = {
   [CLIENT_APP_ROUTES.LOGIN]: '',
   [CLIENT_APP_ROUTES.SIGNUP]: '회원가입',
   [CLIENT_APP_ROUTES.FIND_ID]: '아이디 찾기',
+  [CLIENT_APP_ROUTES.FIND_ID_RESULT]: '아이디 찾기',
   [CLIENT_APP_ROUTES.FIND_PASSWORD]: '비밀번호 찾기',
   [CLIENT_APP_ROUTES.RESET_PASSWORD]: '비밀번호 재설정',
 };
