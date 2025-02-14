5f67831c111151c743eef2ecf9d1dd0969882875
heum2 <heum2.dev@gmail.com>
2025-02-15 00:16:07 +0900

    Fix: searchParams에 Suspense 사용

---
+++ src/components/Spin/index.tsx
@@ -0,0 +1,8 @@
+const Spin = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-16 h-16 border-4 border-gray-300 border-t-primary rounded-full animate-spin"></div>
+    </div>
+  );
+};
+export default Spin;
