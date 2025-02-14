ba861099c9a1b0a7bebe7edc5a5aa668aa8d91e3
채종원 <cowhddnjsdl0@naver.com>
2025-02-14 14:21:52 +0900

    Refactor 1. id 찾기 요청시 분기점 처리 2. 에러코드 추가

--- src/app/(auth)/find-id/apis/postFindId.ts
+++ src/app/(auth)/find-id/apis/postFindId.ts
@@ -2,10 +2,10 @@ import { fetchPost } from '@/apis/fetch';
 
 import { API_ROUTES } from '@/constants/routes';
 
-import { PostFindIdParams } from '@/app/(auth)/find-id/apis/types';
+import { FindIdDto, PostFindIdParams } from '@/app/(auth)/find-id/apis/types';
 
 const postFindId = async (params: PostFindIdParams) => {
-  const response = await fetchPost<string, PostFindIdParams>(
+  const response = await fetchPost<FindIdDto, PostFindIdParams>(
     API_ROUTES.FIND.FIND_ID,
     params,
   );
