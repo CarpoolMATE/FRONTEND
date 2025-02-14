a6195680ad9cbd37ffd77d5607ceff138cd7669e
채종원 <cowhddnjsdl0@naver.com>
2025-02-14 13:45:23 +0900

    feat : 아이디 찾기 api 연동

---
+++ src/app/(auth)/find-id/components/Form/schema.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+export const findIdSchema = z.object({
+  nickname: z.string().min(1, { message: '닉네임을 입력해주세요.' }),
+  email: z.string().min(1, { message: '이메일을 입력해주세요.' }),
+});
+
+export type FindIdFormValues = z.infer<typeof findIdSchema>;
