9b7ac43d5dabb86cf1c62a288ae556450064d625
채종원 <cowhddnjsdl0@naver.com>
2025-02-14 15:19:41 +0900

    Fix 1. 가로 padding 20 > 16으로 통일

--- src/app/(auth)/find-id/page.tsx
+++ src/app/(auth)/find-id/page.tsx
@@ -2,7 +2,7 @@ import FindIdForm from '@/app/(auth)/find-id/components/Form';
 
 const IdFindPage = () => {
   return (
-    <section className="pt-16 px-5 pb-4 h-[calc(100%-64px)]">
+    <section className="pt-16 px-4 pb-4 h-[calc(100%-64px)]">
       <FindIdForm />
     </section>
   );
