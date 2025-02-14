000dec5bff20cde09baa98b84b3de54031e51037
채종원 <cowhddnjsdl0@naver.com>
2025-02-14 13:47:14 +0900

    fixed : 컴포넌트 누락 반영

--- src/app/(auth)/find-id/page.tsx
+++ src/app/(auth)/find-id/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import FindIdForm from '@/app/(auth)/find-id/components/Form';
+
 const IdFindPage: React.FC = () => {
   return (
     <section className="pt-16 px-5 pb-[34px] h-[calc(100%-64px)]">
-      {/* <FindIdForm /> */}
+      <FindIdForm />
     </section>
   );
 };
