38b4e2ba8a16d810e66812c12b2f921bf4bfeec3
채종원 <cowhddnjsdl0@naver.com>
2025-02-15 12:13:37 +0900

    Feat : 회원가입시 중복검사 api를 연동했습니다.

--- src/app/(auth)/sign-up/apis/postCheckDuplicate.ts
+++ src/app/(auth)/sign-up/apis/postCheckDuplicate.ts
@@ -2,17 +2,26 @@ import { fetchPost } from '@/apis/fetch';
 
 import {
   CheckDuplicate,
+<<<<<<< HEAD
   CheckDuplicateDto,
   PostCheckDuplicateParams,
 } from '@/app/(auth)/sign-up/apis/types';
 
+=======
+  PostCheckDuplicateParams,
+} from '@/app/(auth)/sign-up/apis/types';
+>>>>>>> 8d9e342 (Feat : 회원가입시 중복검사 api를 연동했습니다.)
 import { CHECK_DUPLICATE_ROUTE_CLASSES } from '@/app/(auth)/sign-up/apis/constants';
 
 const postCheckDuplicate = async (
   params: PostCheckDuplicateParams,
   type: CheckDuplicate,
 ) => {
+<<<<<<< HEAD
   const response = await fetchPost<CheckDuplicateDto, PostCheckDuplicateParams>(
+=======
+  const response = await fetchPost<boolean, PostCheckDuplicateParams>(
+>>>>>>> 8d9e342 (Feat : 회원가입시 중복검사 api를 연동했습니다.)
     CHECK_DUPLICATE_ROUTE_CLASSES[type],
     params,
   );
