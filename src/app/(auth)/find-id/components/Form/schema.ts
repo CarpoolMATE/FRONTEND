9ddd92388254d5b8dbfc0ad1204867c4dea28d99
채종원 <cowhddnjsdl0@naver.com>
2025-02-15 14:53:16 +0900

    Refactor : find-id, find-password 이메일 형식 확인 로직 추가

--- src/app/(auth)/find-id/components/Form/schema.ts
+++ src/app/(auth)/find-id/components/Form/schema.ts
@@ -2,7 +2,10 @@ import { z } from 'zod';
 
 export const findIdSchema = z.object({
   nickname: z.string().min(1, { message: '닉네임을 입력해주세요.' }),
-  email: z.string().min(1, { message: '이메일을 입력해주세요.' }),
+  email: z
+    .string()
+    .min(1, { message: '이메일을 입력해주세요.' })
+    .email({ message: '유효한 이메일을 입력해주세요.' }),
 });
 
 export type FindIdFormValues = z.infer<typeof findIdSchema>;
