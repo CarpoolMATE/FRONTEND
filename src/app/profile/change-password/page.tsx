ee07f6b67d2b7a57c1ecd141901db9c68a4be553
채종원 <cowhddnjsdl0@naver.com>
2025-02-17 18:53:40 +0900

    Feat/Design : 기존의 프로필 수정 페이지를 삭제하고 드라이버와 승객의 수정페이지를 분리 후 디자인했습니다. / 비밀번호 확인 페이지와 비밀번호 변경 페이지를 디자인 했습니다.

---
+++ src/app/profile/change-password/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Input from '@/components/Input';
+import Button from '@/components/Button';
+import Header from '@/app/(auth)/components/Header';
+
+const ProfileChangePasswordPage = () => {
+  return (
+    <section className="w-full flex flex-col h-screen">
+      <Header />
+      <form className="flex h-full flex-col justify-between p-4 w-full mt-16">
+        <div className="w-full flex flex-col items-center gap-10">
+          <div className="w-full flex flex-col gap-2">
+            <div className="text-sm">비밀번호</div>
+            <div className="flex gap-2 w-full [&>*]:w-full">
+              <Input type="password" placeholder="비밀번호를 입력해주세요" />
+            </div>
+          </div>
+          <div className="w-full flex flex-col gap-2">
+            <div className="text-sm">비밀번호 확인</div>
+            <div className="flex gap-2 w-full [&>*]:w-full">
+              <Input
+                type="password"
+                placeholder="비밀번호를 재입력 해주세요."
+              />
+            </div>
+          </div>
+        </div>
+        <Button>확인</Button>
+      </form>
+    </section>
+  );
+};
+
+export default ProfileChangePasswordPage;
