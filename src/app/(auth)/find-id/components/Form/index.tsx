a284cc38d28512b2813df07e88cbb296293e3153
채종원 <cowhddnjsdl0@naver.com>
2025-02-17 12:33:01 +0900

    Feat : find-id 페이지에 에러시 모달을 적용했습니다.

--- src/app/(auth)/find-id/components/Form/index.tsx
+++ src/app/(auth)/find-id/components/Form/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/navigation';
@@ -18,6 +18,8 @@ import {
   findIdSchema,
 } from '@/app/(auth)/find-id/components/Form/schema';
 
+import Modal from '@/components/Modal';
+
 const FindIdForm = () => {
   const router = useRouter();
 
@@ -29,6 +31,8 @@ const FindIdForm = () => {
   } = useForm<FindIdFormValues>({
     resolver: zodResolver(findIdSchema),
   });
+
+  const [errorText, setErrorText] = useState('');
 
   const { mutateAsync: postFindId } = usePostFindId();
 
@@ -42,14 +46,7 @@ const FindIdForm = () => {
         );
       } catch (error) {
         if (error instanceof Error) {
-          if (error.cause === ERROR_CODE['ACCOUNT-001']) {
-            setError('email', { type: 'validate', message: error.message });
-          } else if (error.cause === ERROR_CODE['ACCOUNT-008']) {
-            setError('nickname', { type: 'validate', message: error.message });
-          } else {
-            //TODO: modal 적용
-            alert(error.message);
-          }
+          setErrorText(error.message);
         }
       }
     },
@@ -57,34 +54,41 @@ const FindIdForm = () => {
   );
 
   return (
-    <form
-      onSubmit={handleSubmit(handleFindId)}
-      className="flex flex-col h-full justify-between"
-    >
-      <div className="flex flex-col gap-5">
-        <div className="flex flex-col gap-[10px]">
-          <h2 className="text-icon text-[14px]">닉네임</h2>
-          <Input
-            placeholder="이름을 입력해주세요"
-            {...register('nickname')}
-            error={!!errors.nickname || !!errors.email}
-            errorText={errors.nickname?.message}
-          />
+    <div className="w-full h-full">
+      <Modal
+        isOpen={!!errorText}
+        message={errorText}
+        onClose={() => setErrorText('')}
+      />
+      <form
+        onSubmit={handleSubmit(handleFindId)}
+        className="flex flex-col h-full justify-between"
+      >
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-[10px]">
+            <h2 className="text-icon text-[14px]">닉네임</h2>
+            <Input
+              placeholder="닉네임을 입력해주세요"
+              {...register('nickname')}
+              error={!!errors.nickname || !!errors.email}
+              errorText={errors.nickname?.message}
+            />
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <h2 className="text-icon text-[14px]">이메일</h2>
+            <Input
+              placeholder="이메일을 입력해주세요."
+              {...register('email')}
+              error={!!errors.email}
+              errorText={errors.email?.message}
+            />
+          </div>
         </div>
-        <div className="flex flex-col gap-[10px]">
-          <h2 className="text-icon text-[14px]">이메일</h2>
-          <Input
-            placeholder="이메일을 입력해주세요."
-            {...register('email')}
-            error={!!errors.email}
-            errorText={errors.email?.message}
-          />
-        </div>
-      </div>
-      <Button disabled={!isValid} className="mb-0">
-        아이디 찾기
-      </Button>
-    </form>
+        <Button disabled={!isValid} className="mb-0">
+          아이디 찾기
+        </Button>
+      </form>
+    </div>
   );
 };
 
