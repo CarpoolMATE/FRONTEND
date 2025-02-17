668f14beadd26d38a0fb1b09ce94da085a212f50
채종원 <cowhddnjsdl0@naver.com>
2025-02-17 13:29:38 +0900

    Fix : 비밀번호 찾기페이지에 변경된 dto 적용 및 공용 모달을 추가했습니다.

--- src/app/(auth)/find-password/components/Form/index.tsx
+++ src/app/(auth)/find-password/components/Form/index.tsx
@@ -1,20 +1,26 @@
 'use client';
 
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
 
 import Input from '@/components/Input';
 import Button from '@/components/Button';
 
 import usePostFindPassword from '@/app/(auth)/find-password/apis/usePostFindPassword';
 
+import { CLIENT_APP_ROUTES } from '@/constants/routes';
+
 import {
   FindPasswordFormValues,
   findPasswordSchema,
 } from '@/app/(auth)/find-password/components/Form/schema';
+import Modal from '@/components/Modal';
 
 const FindPasswordForm = () => {
+  const router = useRouter();
+
   const {
     register,
     handleSubmit,
@@ -26,15 +32,16 @@ const FindPasswordForm = () => {
 
   const { mutateAsync: postFindPassword } = usePostFindPassword();
 
+  const [errorText, setErrorText] = useState('');
+
   const handleFindPassword = useCallback(
     async ({ email, memberId }: FindPasswordFormValues) => {
       try {
         const response = await postFindPassword({ email, memberId });
-        console.log(response);
+        setErrorText(response.message);
       } catch (error) {
         if (error instanceof Error) {
-          //TODO: modal 적용
-          alert(error.message);
+          setErrorText(error.message);
         }
       }
     },
@@ -42,34 +49,45 @@ const FindPasswordForm = () => {
   );
 
   return (
-    <form
-      onSubmit={handleSubmit(handleFindPassword)}
-      className="flex flex-col h-full justify-between"
-    >
-      <div className="flex flex-col gap-5">
-        <div className="flex flex-col gap-[10px]">
-          <h2 className="text-icon text-[14px]">아이디</h2>
-          <Input
-            placeholder="아이디를 입력해주세요"
-            {...register('memberId')}
-            error={!!errors.memberId || !!errors.email}
-            errorText={errors.memberId?.message}
-          />
+    <div className="w-full h-full">
+      <Modal
+        isOpen={!!errorText}
+        message={errorText}
+        onClose={() =>
+          errorText === '이메일이 발송되었습니다.'
+            ? router.push(CLIENT_APP_ROUTES.HOME)
+            : setErrorText('')
+        }
+      />
+      <form
+        onSubmit={handleSubmit(handleFindPassword)}
+        className="flex flex-col h-full justify-between"
+      >
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-[10px]">
+            <h2 className="text-icon text-[14px]">아이디</h2>
+            <Input
+              placeholder="아이디를 입력해주세요"
+              {...register('memberId')}
+              error={!!errors.memberId || !!errors.email}
+              errorText={errors.memberId?.message}
+            />
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <h2 className="text-icon text-[14px]">이메일</h2>
+            <Input
+              placeholder="이메일을 입력해주세요."
+              {...register('email')}
+              error={!!errors.email}
+              errorText={errors.email?.message}
+            />
+          </div>
         </div>
-        <div className="flex flex-col gap-[10px]">
-          <h2 className="text-icon text-[14px]">이메일</h2>
-          <Input
-            placeholder="이메일을 입력해주세요."
-            {...register('email')}
-            error={!!errors.email}
-            errorText={errors.email?.message}
-          />
-        </div>
-      </div>
-      <Button disabled={!isValid} className="mb-0">
-        비밀번호 찾기
-      </Button>
-    </form>
+        <Button disabled={!isValid} className="mb-0">
+          비밀번호 찾기
+        </Button>
+      </form>
+    </div>
   );
 };
 
