4348ad42ba251fbcf5f008156d3e894b1d9ec8f1
heum2 <heum2.dev@gmail.com>
2025-03-09 21:15:07 +0900

    Fix: useSearchParams, Suspense 컴포넌트 설정

--- src/app/(client)/home/page.tsx
+++ src/app/(client)/home/page.tsx
@@ -1,16 +1,26 @@
+import { Suspense } from 'react';
+
 import ReservationCard from '@/app/(client)/home/components/ReservationCard';
 import CarpoolFilter from '@/app/(client)/home/components/CarpoolFilter';
 import CarpoolList from '@/app/(client)/home/components/CarpoolList';
-
 import HomeHeader from '@/app/(client)/home/components/Header';
+import Spin from '@/components/Spin';
 
 const HomePage = () => {
   return (
     <section className="px-5 flex flex-col justify-center items-center">
       <HomeHeader />
       <ReservationCard />
-      <CarpoolFilter />
-      <CarpoolList />
+      <Suspense
+        fallback={
+          <div className="flex items-center h-[30dvh]">
+            <Spin />
+          </div>
+        }
+      >
+        <CarpoolFilter />
+        <CarpoolList />
+      </Suspense>
     </section>
   );
 };
