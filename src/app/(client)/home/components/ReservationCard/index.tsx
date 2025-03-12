c6c23de8f02de6ae2aeb02ef58b37ef1f736e103
heum2 <heum2.dev@gmail.com>
2025-03-13 00:34:01 +0900

    Feat: 예약 상태 카드 - API 연결

--- src/app/(client)/home/components/ReservationCard/index.tsx
+++ src/app/(client)/home/components/ReservationCard/index.tsx
@@ -1,13 +1,18 @@
 'use client';
 
+import Link from 'next/link';
+import { format } from 'date-fns';
+
 import Icon from '@/components/Icon';
 
-import { useMemberStore } from '@/store/member';
+import { CLIENT_APP_ROUTES } from '@/constants/routes';
+
+import { useGetReservationDetail } from '@/app/(client)/reservation-detail/apis/getReservationDetail';
 
 const ReservationCard = () => {
-  const { member } = useMemberStore();
+  const { data: reservation } = useGetReservationDetail();
 
-  if (!member?.reservation) {
+  if (!reservation) {
     return (
       <div className="w-full mt-2 min-h-[147px] bg-white rounded-[1.25rem] shadow-[4px_4px_20px_0px_rgba(0,0,0,0.08)] border border-input flex-col justify-center items-center gap-4 inline-flex">
         <h2 className="text-lg font-semibold">
@@ -20,36 +25,44 @@ const ReservationCard = () => {
     );
   }
 
-  // TODO: 데이터 조회 예정
+  const { carpoolInfo } = reservation;
+  const { departureTime, departureCoordinate } = carpoolInfo;
+
   return (
-    <div className="w-full mt-2 min-h-[147px] bg-white rounded-[1.25rem] shadow-[4px_4px_20px_0px_rgba(0,0,0,0.08)] border border-input flex-col justify-center items-center gap-4 inline-flex">
-      <h2 className="text-lg font-semibold">
-        내일 <span className="text-primary">8시 30분</span>에 카풀 예약이 있어요
-      </h2>
+    <Link href={CLIENT_APP_ROUTES.RESERVATION_DETAIL} className="w-full">
+      <div className="w-full mt-2 min-h-[147px] bg-white rounded-[1.25rem] shadow-[4px_4px_20px_0px_rgba(0,0,0,0.08)] border border-input flex-col justify-center items-center gap-4 inline-flex">
+        <h2 className="text-lg font-semibold">
+          내일{' '}
+          <span className="text-primary">
+            {format(departureTime, 'HH시 mm분')}
+          </span>
+          에 카풀 예약이 있어요
+        </h2>
 
-      <div className="flex items-center gap-9">
-        <div className="flex flex-col items-center gap-y-3">
-          <p className="text-placeholder text-sm font-medium">출발지</p>
-          <p className="text-sm">{'{주소}'}</p>
-        </div>
-        <div className="flex items-center gap-0.5">
-          {Array(3)
-            .fill('')
-            .map((_value, index) => (
-              <Icon
-                key={index}
-                icon="CHEVRONS_RIGHT"
-                className="size-3 text-primary opacity-50"
-              />
-            ))}
-        </div>
+        <div className="flex items-center gap-9">
+          <div className="flex flex-col items-center gap-y-3 text-sm font-medium">
+            <p className="text-placeholder">출발지</p>
+            <p>{departureCoordinate.split(' ').slice(2).join(' ')}</p>
+          </div>
+          <div className="flex items-center gap-0.5">
+            {Array(3)
+              .fill('')
+              .map((_value, index) => (
+                <Icon
+                  key={index}
+                  icon="CHEVRONS_RIGHT"
+                  className="size-3 text-primary opacity-50"
+                />
+              ))}
+          </div>
 
-        <div className="flex flex-col items-center gap-y-3">
-          <p className="text-placeholder text-sm font-medium">도착지</p>
-          <p className="text-sm">경운대학교</p>
+          <div className="flex flex-col items-center gap-y-3 text-sm font-medium">
+            <p className="text-placeholder">도착지</p>
+            <p>경운대학교</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
