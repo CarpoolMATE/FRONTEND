9a6b281bda953c6f24a917399f3dc7ca08c0b299
heum2 <heum2.dev@gmail.com>
2025-03-18 15:12:15 +0900

    Fix: 리스트가 화면 밖으로 나오는 이슈 수정

--- src/components/Layout/RootLayout/index.tsx
+++ src/components/Layout/RootLayout/index.tsx
@@ -7,7 +7,7 @@ import { GlobalModal } from '@/components/Modal';
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <ReactQueryProvider>
-      <section className="max-w-[768px] my-0 mx-auto bg-white h-screen">
+      <section className="max-w-[768px] my-0 mx-auto h-screen">
         {children}
         <GlobalModal />
       </section>
